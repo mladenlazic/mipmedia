43bdcd4d122ee11a54671a381a8a905727ec0087
Mladen <blackmaster_1993@hotmail.com>
2017-02-16 16:15:15 +0100

    add header

--- index.php
+++ index.php
@@ -1,138 +1,119 @@
 <!doctype html>
 <!--pravimo prvi korak u angularu definisemo controler i vidimo kako se koristi-->
 <html ng-app="mipmedia">
-	<head>
-		<meta charset="utf-8">
 
-		<title>mip media</title>
-		<!--ukljucujemo angularjs biblioteku-->
-		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.32/angular.min.js"></script>
-		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.32/angular-route.js"></script>
-		
-		<script type="text/javascript" src="mipmedia.js"></script>
-		<script type="text/javascript" src="controller/offersController.js"></script>
-		<script type="text/javascript" src="controller/homeController.js"></script>
-		<script type="text/javascript" src="controller/aboutController.js"></script>
-		<script type="text/javascript" src="controller/contactController.js"></script>
+<head>
+    <meta charset="utf-8">
 
-				<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <title>mip media</title>
+    <!--ukljucujemo angularjs biblioteku-->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.32/angular.min.js"></script>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.32/angular-route.js"></script>
 
+    <script type="text/javascript" src="mipmedia.js"></script>
+    <script type="text/javascript" src="controller/offersController.js"></script>
+    <script type="text/javascript" src="controller/homeController.js"></script>
+    <script type="text/javascript" src="controller/aboutController.js"></script>
+    <script type="text/javascript" src="controller/contactController.js"></script>
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 
-	<link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
 
-		<link rel="stylesheet" href="view/style.css">
+    <link rel="stylesheet" href="view/style.css">
 
-	</head>
+</head>
 
-	<body>
+<body>
 
-<div class="header-distributed">
+    <div class="header-distributed">
+        <nav class="navbar-inverse">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-3">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">MIPMEDIA</a>
+                </div>
 
-    
-    
-    <!-- Second navbar for search -->
-    <nav class="navbar-inverse">
-      <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-3">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">MIPMEDIA</a>
+                <div class="collapse navbar-collapse" id="navbar-collapse-3">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#/">Home</a></li>
+                        <li><a href="#/offers">Offers</a></li>
+                        <li><a href="#/about">About</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <div ng-view id="container">
+
+    </div>
+
+    <footer class="footer-distributed">
+
+        <div class="footer-left">
+
+            <h3>Company<span>logo</span></h3>
+
+            <p class="footer-links">
+                <a href="#">Home</a> ·
+                <a href="#">Blog</a> ·
+                <a href="#">Pricing</a> ·
+                <a href="#">About</a> ·
+                <a href="#">Faq</a> ·
+                <a href="#">Contact</a>
+            </p>
+
+            <p class="footer-company-name">Company Name &copy; 2015</p>
         </div>
-    
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="navbar-collapse-3">
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#/">Home</a></li>
-            <li><a href="#/offers">Offers</a></li>
-            <li><a href="#/about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container -->
-    </nav><!-- /.navbar -->
-    
-   
-</div><!-- /.container-fluid -->
 
-		<div ng-view id="container">
+        <div class="footer-center">
 
-		</div>
+            <div>
+                <i class="fa fa-map-marker"></i>
+                <p><span>21 Revolution Street</span> Paris, France</p>
+            </div>
 
+            <div>
+                <i class="fa fa-phone"></i>
+                <p>+1 555 123456</p>
+            </div>
 
-<footer class="footer-distributed">
+            <div>
+                <i class="fa fa-envelope"></i>
+                <p><a href="mailto:support@company.com">support@company.com</a></p>
+            </div>
 
-			<div class="footer-left">
+        </div>
 
-				<h3>Company<span>logo</span></h3>
+        <div class="footer-right">
 
-				<p class="footer-links">
-					<a href="#">Home</a>
-					·
-					<a href="#">Blog</a>
-					·
-					<a href="#">Pricing</a>
-					·
-					<a href="#">About</a>
-					·
-					<a href="#">Faq</a>
-					·
-					<a href="#">Contact</a>
-				</p>
+            <p class="footer-company-about">
+                <span>About the company</span> Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+            </p>
 
-				<p class="footer-company-name">Company Name &copy; 2015</p>
-			</div>
+            <div class="footer-icons">
 
-			<div class="footer-center">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-github"></i></a>
 
-				<div>
-					<i class="fa fa-map-marker"></i>
-					<p><span>21 Revolution Street</span> Paris, France</p>
-				</div>
+            </div>
 
-				<div>
-					<i class="fa fa-phone"></i>
-					<p>+1 555 123456</p>
-				</div>
+        </div>
 
-				<div>
-					<i class="fa fa-envelope"></i>
-					<p><a href="mailto:support@company.com">support@company.com</a></p>
-				</div>
+    </footer>
 
-			</div>
+</body>
 
-			<div class="footer-right">
-
-				<p class="footer-company-about">
-					<span>About the company</span>
-					Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
-				</p>
-
-				<div class="footer-icons">
-
-					<a href="#"><i class="fa fa-facebook"></i></a>
-					<a href="#"><i class="fa fa-twitter"></i></a>
-					<a href="#"><i class="fa fa-linkedin"></i></a>
-					<a href="#"><i class="fa fa-github"></i></a>
-
-				</div>
-
-			</div>
-
-		</footer>
-
-
-
-
-	</body>
 </html>
